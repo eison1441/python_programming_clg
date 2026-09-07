b953035492f9a7217d2672d65760878ddcf9e3b6
Eison <eisondanieljohnson007@gmail.com>
2026-09-07 12:15:41 +0530

    c2

--- python/co2_4.py
+++ python/co2_4.py
@@ -1,1 +1,7 @@
-# genarate a list of 4 digit number in a giveen range with all there digit even and the number is a prefect square.
+# genarate a list of 4 digit number in a giveen range with all there digit even and the number is a prefect square without using function
+import math 
+for i in range(1000,10000):
+    sqroot= int(math.sqrt(i))
+    if sqroot*sqroot==i:
+        if all (int(digit) % 2 == 0 for digit in str(i)):
+            print(i)
