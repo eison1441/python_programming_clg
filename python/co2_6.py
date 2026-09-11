2b398064381e913f72d4fe75405fea3d3f350ffc
Eison <eisondanieljohnson007@gmail.com>
2026-09-11 11:40:09 +0530

    problem6

--- python/co2_6.py
+++ python/co2_6.py
@@ -1,3 +1,9 @@
-#Count the number of characters(character frequency) in a string
-a=input("Enter a word =>")
-print("number of characters",len(a))
+# count the number of characters in a string
+string=input("Enter a string:")
+d={}
+for i in string:
+    if i in d:
+        d[i]+-1
+    else:
+        d[i]=1
+print("character frequency = ",d)
