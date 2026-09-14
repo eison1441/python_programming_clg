5a378fcebebfe2e63d4b6df06bd4d43e6ba2b1bd
Eison <eisondanieljohnson007@gmail.com>
2026-09-14 12:23:31 +0530

    DS added

---
+++ Ds/sum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int num1,num2,sum;
+	printf("Enter two Integer:");
+	scanf("%d %d",&num1,&num2);
+	sum=num1+num2;
+	printf("Sum of %d and %d=%d \n",num1,num2,sum);
+	return 0;
+}
