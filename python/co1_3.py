5e0c07df1b2152858d00d30268debd84b26ecd2a
Eison daniel johnson <eisondanieljohnson007@gmail.com>
2026-09-19 15:46:02 +0530

    Add files via upload

--- python/co1_3.py
+++ python/co1_3.py
@@ -1,19 +1,21 @@
 # List comprehensions
 # a) generate positive list of numbers from a given list of integers
 
-numbers=[1,2,3,4,-5,6,-7,8,-9,10]
-positive_numbers=[x for x in numbers if x>0]
-print("positive numbers :",positive_numbers)
-
-
-
+# numbers=[1,2,3,4,-5,6,-7,8,-9,10]
+# positive_numbers=[x for x in numbers if x>0]
+# print("positive numbers :",positive_numbers)
 
 
 # b) square of N numbers
+limit=int(input("enter a range"))
+squares=[x*x for x in range(1,limit)]
+print(squares)
 
 
 
+# c)From a list of vowels selected from a given word using list cpmprehension
 
-# c)From a list of vowels selected from a given word
+
+
 
 # d) List ordinal value of each element of a word
